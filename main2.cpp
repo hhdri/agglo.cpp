873ba0181ded0f9f8494e728ab066f35b4abc960
Majid Hajiheidari <majid.hajiheidari@divar.ir>
2023-12-25 23:31:39 +0100

    Add faiss search

--- main2.cpp
+++ main2.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <thread>
+#include <chrono>
+
+#include <faiss/IndexFlat.h>
 
 using std::string, std::cout, std::endl;
 
 const int VOCAB_SIZE = 400'000;
-const int EMBEDDING_DIM = 300;
-static float embeddings[VOCAB_SIZE][EMBEDDING_DIM];
+const int EMBEDDING_DIM = 50;
+static float embeddings[VOCAB_SIZE * EMBEDDING_DIM];
 static string vocab[VOCAB_SIZE];
 
 struct Partition {
@@ -17,7 +19,6 @@ struct Partition {
 
 void loadPartition(const Partition &partition, int offset) {
     std::ifstream file(partition.path);
-
     for (int i = offset; i < offset + partition.size; i++) {
         string line, word;
         std::getline(file, line);
@@ -25,54 +26,49 @@ void loadPartition(const Partition &partition, int offset) {
         lineStream >> word;
         vocab[i] = word;
 
+        float normSquared = 0.0;
         for (int j = 0; j < EMBEDDING_DIM; j++) {
-            lineStream >> embeddings[i][j];
+            float value;
+            lineStream >> value;
+            embeddings[i * EMBEDDING_DIM + j] = value;
+            normSquared += value * value;
+        }
+        float norm = sqrt(normSquared);
+        for (int j = 0; j < EMBEDDING_DIM; j++) {
+            embeddings[i * EMBEDDING_DIM + j] /= norm;
         }
     }
-
     file.close();
 }
 
 int main() {
-    const string pathPrefix = "/Users/majid/Downloads/glove.6B/glove.6B.300d/glove.6B.300d.";
-    const Partition partitions[] = {
-            {pathPrefix + "aa", 50'000},
-            {pathPrefix + "ab", 50'000},
-            {pathPrefix + "ac", 50'000},
-            {pathPrefix + "ad", 50'000},
-            {pathPrefix + "ae", 50'000},
-            {pathPrefix + "af", 50'000},
-            {pathPrefix + "ag", 50'000},
-            {pathPrefix + "ah", 50'000},
-    };
+    loadPartition({"/Users/majid/Downloads/glove.6B/glove.6B.50d.txt", 400'000}, 0);
 
-//    std::thread threads[8];
-//
-//    for (int i = 0; i < 8; i++) {
-//        threads[i] = std::thread(loadPartition, partitions[i], i * 50'000);
+    auto start = std::chrono::high_resolution_clock::now();
+
+    faiss::IndexFlatIP index(EMBEDDING_DIM);
+    index.add(VOCAB_SIZE, embeddings);
+
+    const int SEARCH_SIZE = 50'000;
+
+    auto *searchIndices = new long long[SEARCH_SIZE * 5];
+    auto *searchDistances = new float[SEARCH_SIZE * 5];
+
+    index.search(SEARCH_SIZE, embeddings, 5, searchDistances, searchIndices);
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    cout << "Duration: " << duration << "ms" << endl;
+
+    // Duration: 14618ms
+
+//    for (int i = 0; i < SEARCH_SIZE; i++) {
+//        cout << vocab[i] << endl;
+//        for (int j = 0; j < 5; j++) {
+//            cout << "\t" << vocab[searchIndices[i * 5 + j]] << " " << searchDistances[i * 5 + j] << endl;
+//        }
 //    }
-//
-//    for (auto &thread: threads) {
-//        thread.join();
-//    }
-
-//    loadPartition(partitions[0], 0);
-//    loadPartition(partitions[1], 50'000);
-//    loadPartition(partitions[2], 100'000);
-//    loadPartition(partitions[3], 150'000);
-//    loadPartition(partitions[4], 200'000);
-//    loadPartition(partitions[5], 250'000);
-//    loadPartition(partitions[6], 300'000);
-//    loadPartition(partitions[7], 350'000);
-
-    loadPartition({"/Users/majid/Downloads/glove.6B/glove.6B.300d.txt", 400'000}, 0);
-
-    cout << vocab[0] << endl;
-    cout << embeddings[0][EMBEDDING_DIM - 1] << endl;
-
-    int idx = 359685;
-    cout << vocab[idx] << endl;
-    cout << embeddings[idx][2] << endl;
 
     cout << "Done" << endl;
 }
